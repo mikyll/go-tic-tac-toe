51cf4324c5ad7872dd7de888eadbb85b44d05cd0
Michele Righi <righi.michy@gmail.com>
2022-01-08 01:11:41 +0100

    update

---
+++ app.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"syscall"
+
+	"github.com/manifoldco/promptui"
+	"golang.org/x/term"
+)
+
+/*func main() {
+	prompt := promptui.Select{
+		Label: "Select Day",
+		Items: []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
+			"Saturday", "Sunday"},
+	}
+
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+
+	// Create a tic-tac-toe board.
+	board := [][]string{
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+		[]string{"_", "_", "_"},
+	}
+
+	// The players take turns.
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
+}*/
+
+type pepper struct {
+	Name     string
+	HeatUnit int
+	Peppers  int
+}
+
+func main() {
+	if !term.IsTerminal(int(syscall.Stdin)) {
+		fmt.Println("Terminal is not interactive! Consider using flags or environment variables!")
+		return
+	}
+
+	peppers := []pepper{
+		{Name: "Bell Pepper", HeatUnit: 0, Peppers: 0},
+		{Name: "Banana Pepper", HeatUnit: 100, Peppers: 1},
+		{Name: "Poblano", HeatUnit: 1000, Peppers: 2},
+		{Name: "Jalapeño", HeatUnit: 3500, Peppers: 3},
+		{Name: "Aleppo", HeatUnit: 10000, Peppers: 4},
+		{Name: "Tabasco", HeatUnit: 30000, Peppers: 5},
+		{Name: "Malagueta", HeatUnit: 50000, Peppers: 6},
+		{Name: "Habanero", HeatUnit: 100000, Peppers: 7},
+		{Name: "Red Savina Habanero", HeatUnit: 350000, Peppers: 8},
+		{Name: "Dragon’s Breath", HeatUnit: 855000, Peppers: 9},
+	}
+
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ . }}?",
+		Active:   "\U0001F336 {{ .Name | cyan }} ({{ .HeatUnit | red }})",
+		Inactive: "  {{ .Name | cyan }} ({{ .HeatUnit | red }})",
+		Selected: "\U0001F336 {{ .Name | red | cyan }}",
+		Details: `
+--------- Pepper ----------
+{{ "Name:" | faint }}	{{ .Name }}
+{{ "Heat Unit:" | faint }}	{{ .HeatUnit }}
+{{ "Peppers:" | faint }}	{{ .Peppers }}`,
+	}
+
+	searcher := func(input string, index int) bool {
+		pepper := peppers[index]
+		name := strings.Replace(strings.ToLower(pepper.Name), " ", "", -1)
+		input = strings.Replace(strings.ToLower(input), " ", "", -1)
+
+		return strings.Contains(name, input)
+	}
+
+	prompt := promptui.Select{
+		Label:     "Spicy Level",
+		Items:     peppers,
+		Templates: templates,
+		Size:      4,
+		Searcher:  searcher,
+	}
+
+	i, _, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose number %d: %s\n", i+1, peppers[i].Name)
+}
---
+++ go.mod
@@ -0,0 +1,11 @@
+module app
+
+go 1.17
+
+require github.com/manifoldco/promptui v0.9.0
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
+)
